38d586ec41a097ba6448f979c080ffaa40536463
AngelSalgado555 <angel.salgado@educa.madrid.org>
2025-11-12 20:54:55 +0100

    Vamos completando cosas

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,70 +1,16 @@
 <?php
 class Users{
     public function __construct(
+        private array $names,
+        private array $surnames,
+        private array $dnis,
+        private array $carnetss,
         private array $nicksName,
         private array $password,
-        private array $mail
+        private array $emails
     ){}
 
-        /**
-         * Get the value of username
-         */ 
-        public function getUsername()
-        {
-                return $this->nicksName;
-        }
-
-        /**
-         * Set the value of username
-         *
-         * @return  self
-         */ 
-        public function setUsername($username)
-        {
-                $this->nicksName = $username;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of password
-         */ 
-        public function getPassword()
-        {
-                return $this->password;
-        }
-
-        /**
-         * Set the value of password
-         *
-         * @return  self
-         */ 
-        public function setPassword($password)
-        {
-                $this->password = $password;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of mail
-         */ 
-        public function getMail()
-        {
-                return $this->mail;
-        }
-
-        /**
-         * Set the value of mail
-         *
-         * @return  self
-         */ 
-        public function setMail($mail)
-        {
-                $this->mail = $mail;
-
-                return $this;
-        }
+       
     public function addUser($newUser, $newPass, $newMail){
         $users = $this -> nicksName; 
         for ($i=0; $i < count($users); $i++) { 
