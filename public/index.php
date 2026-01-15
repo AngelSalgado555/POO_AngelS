8eb8f64eb7dcf068dfa3b0d5f75383be28cc29cc
= <=>
2026-01-15 12:45:05 +0100

    Tengo que hacer los formularios y después hacer tambien lo de la base de datos de mis clases

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Nuevo Index </title>
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        .container{
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/views/layouts/header.php";
+    ?>
+
+    <div class="container">
+
+    </div>
+
+    <?php
+        require_once $_SERVER['DOCUMENT_ROOT'] . "/resources/views/layouts/footer.php";
+    ?>
+</body>
+</html>
---
+++ resources/views/components/login.php
@@ -0,0 +1,4 @@
+<!-- Formulario de Login -->
+<div class="container">
+    
+</div>
